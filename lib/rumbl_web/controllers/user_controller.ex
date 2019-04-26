550ac74f770a07c92403c9cb72fef5ded1b61165
Alex Tsiras <tsirasalexis@yahoo.gr>
2019-04-26 08:45:42 +0300

    Add Auth.login/2

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -4,16 +4,9 @@ defmodule RumblWeb.UserController do
   alias Rumbl.Accounts
   alias Rumbl.Accounts.User
 
+  plug :authenticate when action in [:index, :show]
+
   def index(conn, _params) do
-    case authenticate(conn) do
-      %Plug.Conn{halted: true} = conn ->
-        conn
-
-      conn ->
-        users = Accounts.list_users()
-        render(conn, "index.html", users: users)
-    end
-
     users = Accounts.list_users()
     render(conn, "index.html", users: users)
   end
@@ -32,6 +25,7 @@ defmodule RumblWeb.UserController do
     case Accounts.register_user(user_params) do
       {:ok, user} ->
         conn
+        |> RumblWeb.Auth.login(user)
         |> put_flash(:info, "#{user.name} created!")
         |> redirect(to: Routes.user_path(conn, :index))
 
@@ -40,7 +34,7 @@ defmodule RumblWeb.UserController do
     end
   end
 
-  def authenticate(conn) do
+  defp authenticate(conn, _opts) do
     if conn.assigns.current_user do
       conn
     else
